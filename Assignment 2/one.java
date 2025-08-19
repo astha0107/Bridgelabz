d2cb512964ffa6c450a5b26f114abd829beea7ae
astha0107 <goswamiastha0107@gmail.com>
2025-08-19 10:00:17 +0530

    msg

--- Assignment 2/one.java
+++ Assignment 2/one.java
@@ -20,21 +20,101 @@
 // }
 
 // ques 2
+// public class one {
+//     public static void main(String[] args) {
+//         Car c1=new Car("xuv 500",2000);
+//         Car c2=new Car("dezire swift",2015);
+//         System.out.println("cars created are "+Car.numofcars);
+//     }
+    
+// }
+// class Car{
+//     String model;
+//     int year;
+//     static int numofcars=0; //this will increment
+//     Car(String model, int year){
+//         this.model=model;
+//         this.year=year;
+//         numofcars++;
+//     }
+// }
+
+// ques 3
+// public class one{
+//     public static void main(String[] args) {
+//         Demo d= new Demo();
+//         d.check();
+//     }
+// }
+// class Demo{
+//     void check(){
+//         int a=20; //local variable
+//         System.out.println("local variable is "+a);
+//     }
+// }
+
+// ques 4
+// public class one{
+//     public static void main(String[] args) {
+//         Student s1=new Student("Astha", 50);
+//         Student s2=new Student("Asha", 40);
+//         System.out.println("Name of student 1 is "+s1.name+" marks scored are "+s1.marks);
+//         System.out.println("Name of student 2 is "+s2.name+" marks scored are "+s2.marks);
+
+
+//     }
+// }
+
+// class Student{
+//     String name;
+//     int marks;
+//     Student(String name, int marks){
+//         this.name=name;
+//         this.marks=marks;
+//     }
+// }
+
+// ques 5
+// public class one {
+
+//     public static void main(String[] args) {
+//         New n1=new New();
+//         n1.a=20;
+//         New n2=new New();
+//         System.out.println("n2 sees count as: "+n2.a);
+//     }
+// }
+// class New{
+//      static int a=0;
+// }
+
+
+// ques 6
+// public class one{
+//     public static void main(String[] args) {
+//         Demo d=new Demo();
+//         d.setvalue(56);
+//         d.show();
+//     }
+// }
+// class Demo{
+//     int value;
+//     void setvalue(int value){
+//         this.value=value;
+//     }
+//     void show(){
+//         System.out.println("instance variable is "+value);
+//     }
+// }
+
+// ques 7
 public class one {
     public static void main(String[] args) {
-        Car c1=new Car("xuv 500",2000);
-        Car c2=new Car("dezire swift",2015);
-        System.out.println("cars created are "+Car.numofcars);
-    }
-    
-}
-class Car{
-    String model;
-    int year;
-    static int numofcars=0; //this will increment
-    Car(String model, int year){
-        this.model=model;
-        this.year=year;
-        numofcars++;
+        Test t=new Test();
+        t.x=20;
     }
 }
+class Test{
+    final int x=80;
+}
+//here compilation problem is that final feild Test.x cannot be resolved.
