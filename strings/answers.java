e29567a5e0dc631f7fb17c5e716b6ccff8a48fa8
astha0107 <goswamiastha0107@gmail.com>
2025-08-25 10:50:31 +0530

    ans 1

---
+++ strings/answers.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class answers {
+    public static void main(String[] args) {
+        //ans
+        String a = "astha";
+        String b = "Astha";
+        
+        if (a.equals(b)) {
+            System.out.println("strings are equal");
+        }else {
+            System.out.println("not equal");
+        }
+    }
+    
+}
