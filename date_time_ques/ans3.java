6547ac47f5c11c91ee121fa0511aaeb7e2cc53de
astha0107 <goswamiastha0107@gmail.com>
2025-08-27 11:08:45 +0530

    ans3

---
+++ date_time_ques/ans3.java
@@ -0,0 +1,23 @@
+package date_time_ques;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ans3 {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss z");
+
+        
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        System.out.println("Current Time in GMT : " + gmtTime.format(formatter));
+
+        
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Time in IST : " + istTime.format(formatter));
+
+        
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println("Current Time in PST : " + pstTime.format(formatter));
+    }
+}
