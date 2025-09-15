390174c18a6bb4c902f5f087788a3fc25ae0dddd
astha0107 <goswamiastha0107@gmail.com>
2025-09-15 10:42:57 +0530

    one

---
+++ oops/basic/one.java
@@ -0,0 +1,22 @@
+package oops.basic;
+
+public class one {
+    public static void main(String[] args) {
+        student s1=new student("alice", 22, 45.9);
+        s1.display();
+
+    }
+}
+class student{
+    String name;
+    int rollno;
+    double marks;
+    student(String name, int rollno, double marks){
+        this.name=name;
+        this.rollno=rollno;
+        this.marks=marks;
+    }
+    public void display(){
+        System.out.println("name is "+name+" roll no. is "+rollno+" marks are "+marks);
+    }
+}
